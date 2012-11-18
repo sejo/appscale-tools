32dce29e13998f162f15757b8e4d350117fabd1b
Roberto Aguilar <roberto@baremetal.io>
2012-11-17 21:37:17 -0800

    Added debian packaging
    These packages are recommended for building .deb files.

--- manifests/base.pp
+++ manifests/base.pp
@@ -3,7 +3,8 @@ Exec { path => ["/usr/bin", "/usr/local/bin", "/bin"] }
 class appscale_dependencies {
   exec { "apt-get update": }
 
-  package { ["openjdk-6-jdk", "vim", "openssh-server", "git-core", "tcsh"]:
+  package { ["build-essential", "debhelper", "dh-make", "fakeroot", "lintian", "gnupg", "pbuilder",
+             "openjdk-6-jdk", "vim", "openssh-server", "git-core", "tcsh"]:
     ensure => present,
     require => Exec["apt-get update"],
   }
