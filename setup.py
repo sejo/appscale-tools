300b50e01614cee4f3e523b6a5a06b205978e4dd
Roberto Aguilar <roberto@baremetal.io>
2012-12-01 03:01:26 -0800

    Added setup.py for installation

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python 
+import os
+
+from distutils.core import setup 
+
+SCRIPT_DIR = os.path.dirname(__file__)
+if SCRIPT_DIR == '':
+    SCRIPT_DIR = '.'
+
+scripts = filter(lambda x: not os.path.basename(x).startswith('.'), ['bin/%s' % x for x in os.listdir('%s/bin' % (SCRIPT_DIR,))])
+
+setup(name = 'appscale-tools', 
+      version = '0.1', 
+      description = "AppScale Management Tools",
+      author = "Roberto Aguilar", 
+      author_email = "roberto@appscale.com", 
+      packages = ['appscaletools'], 
+      long_description=open('README').read(),
+      scripts=scripts,
+      url='http://github.com/AppScale/appscale-tools',
+      license='LICENSE'
+)
