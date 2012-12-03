e63e6ae950bc6e48b1effa12c35e9b536ca489f0
Roberto Aguilar <roberto@baremetal.io>
2012-12-03 12:33:01 -0800

    Added data_files to setup.py.
    Install the appscale-bootstrap-ec2 script to data_files installation.
    When the command is not run from a repository, this file will be used.

--- setup.py
+++ setup.py
@@ -9,6 +9,10 @@ if SCRIPT_DIR == '':
 
 scripts = filter(lambda x: not os.path.basename(x).startswith('.'), ['bin/%s' % x for x in os.listdir('%s/bin' % (SCRIPT_DIR,))])
 
+data_files=[
+        ('share/bootstrap', ['bootstrap/appscale-bootstrap-ec2']),
+]
+
 setup(name = 'appscale-tools', 
       version = '0.1', 
       description = "AppScale Management Tools",
@@ -17,6 +21,7 @@ setup(name = 'appscale-tools',
       packages = ['appscaletools'], 
       long_description=open('README').read(),
       scripts=scripts,
+      data_files=data_files,
       url='http://github.com/AppScale/appscale-tools',
       license='LICENSE'
 )
